27c6005ba9deeb041bb186e542f4ed40a0485189
project2003 <projectalgoritmika123@gmail.com>
2022-10-23 17:43:47 +0400

    added file

---
+++ PurpleBuzz-Backend/Areas/Admin/ViewModels/ContactIntro/ContactIntroIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PurpleBuzz_Backend.Models;
+namespace PurpleBuzz_Backend.Areas.Admin.ViewModels
+{
+    public class ContactIntroIndexViewModel
+    {
+        //public ContactIntroIndexViewModel(List<ContactIntroComponent> contactIntroComponents)
+        //{
+        //    ContactIntroComponents = contactIntroComponents;
+        //}
+
+        public List<ContactIntroComponent> ContactIntroComponents { get; set; }
+    }
+}
---
+++ PurpleBuzz-Backend/Areas/Admin/ViewModels/RecentWork/RecentWorkIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+using PurpleBuzz_Backend.Models;
+namespace PurpleBuzz_Backend.Areas.Admin.ViewModels
+{
+    public class RecentWorkIndexViewModel
+    {
+     
+        public List<RecentWorkComponent>RecentWorkComponents { get; set; }
+    }
+}
